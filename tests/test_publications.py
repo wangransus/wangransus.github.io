e68e0e99f1555bdacfbf71d8297b2057e5061b1a
wangransus <wangranmax@gmail.com>
2026-07-17 10:59:33 +0800

    test: lock publication citation fidelity

--- tests/test_publications.py
+++ tests/test_publications.py
@@ -1,3 +1,4 @@
+from hashlib import sha256
 from pathlib import Path
 import re
 import unittest
@@ -41,26 +42,48 @@ EXPECTED_IMAGE_PATHS = [
     "/images/2018JACN.png",
 ]
 EXPECTED_PUBLICATION_PROJECTION = [
-    ("Sensors 2026", "/images/2026Sensors.jpg", "https://www.mdpi.com/1424-8220/26/4/1161", "/docs/2026Sensors.pdf"),
-    ("\u300a\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280\u300b2026", "/images/2026\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.jpg", "https://zgty.cbpt.cnki.net/portal/journal/portal/client/paper/9aa26310534f11d9ce1b7b03ef8ab6c7", "/docs/2026\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.pdf"),
-    ("Sports 2025", "/images/2025Sports.jpg", "https://www.mdpi.com/2075-4663/13/12/418", "/docs/2025Sports.pdf"),
-    ("\u300a\u533b\u7528\u751f\u7269\u529b\u5b66\u300b2025", "/images/2025\u533b\u7528\u751f\u7269\u529b\u5b66.png", "http://www.mechanobiology.cn/yyswlx/article/abstract/202503007?st=article_issue", "/docs/2025\u533b\u7528\u751f\u7269\u529b\u5b66.pdf"),
-    ("\u300a\u5929\u6d25\u4f53\u80b2\u5b66\u9662\u5b66\u62a5\u300b2025", "/images/2025\u5929\u6d25\u4f53\u80b2\u5b66\u9662\u5b66\u62a5.jpg", "https://kns.cnki.net/kcms2/article/abstract?v=y_SiIdm5mqvqk45ugVBXBLXnO2ofzeVOpFoslaGU-URdyWrw0Z6-uDG1Y1QPke6iOF1loIGdDCXbK-M0w7muh2zVesLp_8Rwv3QCWatoHkoYFEEKpAGK_MW1ZtRsgCxiNJxsWVjSJVV60ddnnAcrPmbC72VQSzWe2evtWa9Xtu0=&uniplatform=NZKPT", "/docs/2025\u5929\u6d25\u4f53\u80b2\u5b66\u9662\u5b66\u62a5.pdf"),
-    ("J Strength Cond Res. 2025", "/images/2025JSCR.jpg", "https://pubmed.ncbi.nlm.nih.gov/40266644/", "/docs/2025JSCR.pdf"),
-    ("Sports Med. 2025", "/images/2025SportsMedicine.png", "https://doi.org/10.1007/s40279-024-02170-6", "/docs/2025SportsMedicine.pdf"),
-    ("\u300a\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280\u300b2025", "/images/2025\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.jpg", "http://www.cisszgty.com/tykj/2025/channel/1/2025/0126/5058.html", "/docs/2025\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.pdf"),
-    ("\u300a\u4f53\u80b2\u79d1\u5b66\u300b2024", "/images/2024\u4f53\u80b2\u79d1\u5b662.jpg", "http://tykx.xml-journal.net/article/doi/10.16469/J.css.2024KX040", "/docs/2024\u4f53\u80b2\u79d1\u5b662.pdf"),
-    ("\u300a\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280\u300b2024", "/images/2024\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.jpg", "http://www.cisszgty.com/tykj/2024/channel/4/2024/0702/4823.html", "/docs/2024\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.pdf"),
-    ("\u300a\u4f53\u80b2\u79d1\u5b66\u300b2024", "/images/2024\u4f53\u80b2\u79d1\u5b66.jpg", "http://tykx.xml-journal.net/article/doi/10.16469/j.css.202401010", "/docs/2024\u4f53\u80b2\u79d1\u5b66.pdf"),
-    ("Sports Med. 2024", "/images/2024SportsMedicine2.png", "https://link.springer.com/article/10.1007/s40279-024-02025-0", "/docs/2024SportsMedicine2.pdf"),
-    ("Sports Med. 2024", "/images/2024SportsMedicine.jpg", "https://link.springer.com/article/10.1007/s40279-024-02003-6", "/docs/2024SportsMedicine.pdf"),
-    ("\u300a\u4f53\u80b2\u79d1\u5b66\u300b2023", "/images/2023\u4f53\u80b2\u79d1\u5b66.jpg", "http://tykx.xml-journal.net/article/doi/10.16469/j.css.202301005", "/docs/2023\u4f53\u80b2\u79d1\u5b66.pdf"),
-    ("\u300a\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280\u300b2022", "/images/2022\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.jpg", "http://www.cisszgty.com/tykj/2022/channel/11/2023/0131/4307.html", "/docs/2022\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.pdf"),
-    ("\u300a\u6210\u90fd\u4f53\u80b2\u5b66\u9662\u5b66\u62a5\u300b2022", "/images/2021\u6210\u4f53\u5b66\u62a5.png", "https://dx.doi.org/10.15942/j.jcsu.2022.01.021", "/docs/2022\u6210\u4f53\u5b66\u62a5.pdf"),
-    ("J. Sports Eng. Technol. 2021", "/images/2021JSET.png", "https://dx.doi.org/10.1177/17543371211050312", "/docs/2021JSET.pdf"),
-    ("\u300a\u4f53\u80b2\u4e0e\u79d1\u5b66\u300b2020", "/images/2020\u4f53\u80b2\u4e0e\u79d1\u5b66.png", "https://dx.doi.org/10.13598/j.issn1004-4590.2020.06.012", "/docs/2020\u4f53\u80b2\u4e0e\u79d1\u5b66.pdf"),
-    ("Sports Biomech. 2020", "/images/2020SportsBiomechanics.png", "https://dx.doi.org/10.1080/14763141.2018.1497194", "/docs/2020SportsBiomechanics.pdf"),
-    ("J. Am. Coll. Nutr. 2018", "/images/2018JACN.png", "https://doi.org/10.1080/07315724.2018.1475269", "/docs/2018JACN.pdf"),
+    ("Sensors 2026", "/images/2026Sensors.jpg", "Sensors article cover", "https://www.mdpi.com/1424-8220/26/4/1161", "/docs/2026Sensors.pdf"),
+    ("\u300a\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280\u300b2026", "/images/2026\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.jpg", "\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280 article cover", "https://zgty.cbpt.cnki.net/portal/journal/portal/client/paper/9aa26310534f11d9ce1b7b03ef8ab6c7", "/docs/2026\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.pdf"),
+    ("Sports 2025", "/images/2025Sports.jpg", "Sports article cover", "https://www.mdpi.com/2075-4663/13/12/418", "/docs/2025Sports.pdf"),
+    ("\u300a\u533b\u7528\u751f\u7269\u529b\u5b66\u300b2025", "/images/2025\u533b\u7528\u751f\u7269\u529b\u5b66.png", "\u533b\u7528\u751f\u7269\u529b\u5b66 article cover", "http://www.mechanobiology.cn/yyswlx/article/abstract/202503007?st=article_issue", "/docs/2025\u533b\u7528\u751f\u7269\u529b\u5b66.pdf"),
+    ("\u300a\u5929\u6d25\u4f53\u80b2\u5b66\u9662\u5b66\u62a5\u300b2025", "/images/2025\u5929\u6d25\u4f53\u80b2\u5b66\u9662\u5b66\u62a5.jpg", "\u5929\u6d25\u4f53\u80b2\u5b66\u9662\u5b66\u62a5 article cover", "https://kns.cnki.net/kcms2/article/abstract?v=y_SiIdm5mqvqk45ugVBXBLXnO2ofzeVOpFoslaGU-URdyWrw0Z6-uDG1Y1QPke6iOF1loIGdDCXbK-M0w7muh2zVesLp_8Rwv3QCWatoHkoYFEEKpAGK_MW1ZtRsgCxiNJxsWVjSJVV60ddnnAcrPmbC72VQSzWe2evtWa9Xtu0=&uniplatform=NZKPT", "/docs/2025\u5929\u6d25\u4f53\u80b2\u5b66\u9662\u5b66\u62a5.pdf"),
+    ("J Strength Cond Res. 2025", "/images/2025JSCR.jpg", "Journal of Strength and Conditioning Research article cover", "https://pubmed.ncbi.nlm.nih.gov/40266644/", "/docs/2025JSCR.pdf"),
+    ("Sports Med. 2025", "/images/2025SportsMedicine.png", "Sports Medicine article cover", "https://doi.org/10.1007/s40279-024-02170-6", "/docs/2025SportsMedicine.pdf"),
+    ("\u300a\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280\u300b2025", "/images/2025\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.jpg", "\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280 article cover", "http://www.cisszgty.com/tykj/2025/channel/1/2025/0126/5058.html", "/docs/2025\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.pdf"),
+    ("\u300a\u4f53\u80b2\u79d1\u5b66\u300b2024", "/images/2024\u4f53\u80b2\u79d1\u5b662.jpg", "\u4f53\u80b2\u79d1\u5b66 article cover", "http://tykx.xml-journal.net/article/doi/10.16469/J.css.2024KX040", "/docs/2024\u4f53\u80b2\u79d1\u5b662.pdf"),
+    ("\u300a\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280\u300b2024", "/images/2024\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.jpg", "\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280 article cover", "http://www.cisszgty.com/tykj/2024/channel/4/2024/0702/4823.html", "/docs/2024\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.pdf"),
+    ("\u300a\u4f53\u80b2\u79d1\u5b66\u300b2024", "/images/2024\u4f53\u80b2\u79d1\u5b66.jpg", "\u4f53\u80b2\u79d1\u5b66 article cover", "http://tykx.xml-journal.net/article/doi/10.16469/j.css.202401010", "/docs/2024\u4f53\u80b2\u79d1\u5b66.pdf"),
+    ("Sports Med. 2024", "/images/2024SportsMedicine2.png", "Sports Medicine article cover", "https://link.springer.com/article/10.1007/s40279-024-02025-0", "/docs/2024SportsMedicine2.pdf"),
+    ("Sports Med. 2024", "/images/2024SportsMedicine.jpg", "Sports Medicine article cover", "https://link.springer.com/article/10.1007/s40279-024-02003-6", "/docs/2024SportsMedicine.pdf"),
+    ("\u300a\u4f53\u80b2\u79d1\u5b66\u300b2023", "/images/2023\u4f53\u80b2\u79d1\u5b66.jpg", "\u4f53\u80b2\u79d1\u5b66 article cover", "http://tykx.xml-journal.net/article/doi/10.16469/j.css.202301005", "/docs/2023\u4f53\u80b2\u79d1\u5b66.pdf"),
+    ("\u300a\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280\u300b2022", "/images/2022\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.jpg", "\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280 article cover", "http://www.cisszgty.com/tykj/2022/channel/11/2023/0131/4307.html", "/docs/2022\u4e2d\u56fd\u4f53\u80b2\u79d1\u6280.pdf"),
+    ("\u300a\u6210\u90fd\u4f53\u80b2\u5b66\u9662\u5b66\u62a5\u300b2022", "/images/2021\u6210\u4f53\u5b66\u62a5.png", "\u6210\u90fd\u4f53\u80b2\u5b66\u9662\u5b66\u62a5 article cover", "https://dx.doi.org/10.15942/j.jcsu.2022.01.021", "/docs/2022\u6210\u4f53\u5b66\u62a5.pdf"),
+    ("J. Sports Eng. Technol. 2021", "/images/2021JSET.png", "Journal of Sports Engineering and Technology article cover", "https://dx.doi.org/10.1177/17543371211050312", "/docs/2021JSET.pdf"),
+    ("\u300a\u4f53\u80b2\u4e0e\u79d1\u5b66\u300b2020", "/images/2020\u4f53\u80b2\u4e0e\u79d1\u5b66.png", "\u4f53\u80b2\u4e0e\u79d1\u5b66 article cover", "https://dx.doi.org/10.13598/j.issn1004-4590.2020.06.012", "/docs/2020\u4f53\u80b2\u4e0e\u79d1\u5b66.pdf"),
+    ("Sports Biomech. 2020", "/images/2020SportsBiomechanics.png", "Sports Biomechanics article cover", "https://dx.doi.org/10.1080/14763141.2018.1497194", "/docs/2020SportsBiomechanics.pdf"),
+    ("J. Am. Coll. Nutr. 2018", "/images/2018JACN.png", "Journal of the American Nutrition Association article cover", "https://doi.org/10.1080/07315724.2018.1475269", "/docs/2018JACN.pdf"),
+]
+EXPECTED_CITATION_SHA256 = [
+    "f7c9c57b2b91a7e57f213b86617613dd1d01d88b6eea6a7d145f28ec6b7b5a79",
+    "ff448ea205741fb2cb98e03d75d9deca67f3e3a9756b79fa9b0b6401f398652c",
+    "60f47a4beab553bb4a9b70854f722aebc66c98bde153b20deb1baed3e98dec0a",
+    "112b8dea45bfa15af9c52df6bb2af89ff1c01a26b2b14546bc6bff5a425fa971",
+    "f1af586f6e2dd811566ff63e115db2973c88243c4ba4a779ecaf1a96cde76b12",
+    "d1752fb30259030836f206e4d0302489dd7a4e6057d39befe251ca6bec290c2e",
+    "a59eca632030ad289dec89dde3eb3c588fe8db10802419f711bdeb431ea37e7b",
+    "61375ac452af8b06620bf495d2ca9575a7357966f70c6178b4f4f911a2eb3f18",
+    "4393baf5258990c0c22efbe47cf889df904fa2dd3e53684463804bf3c6ca8960",
+    "4b323d741aad01ccdcc0a4cc1eccf6e7851e05fe358b32f18c330909f8701f9a",
+    "b937474abc60082d549ec1c5fe1e5b612b6e25335779cec2a75203aba3b9f341",
+    "979c19842cbb800d41f84af5f631fcfc7c9fa5bf78039a414b9adb72a620f99d",
+    "84dff4ea580bccdddfb19612ff5e33bc7941ed3410d8974c11ba4f5d4eeedc9d",
+    "1a6d8dd816e8f38940a5dc7b418cfc423fd8908d1f1ff801d5f07163e2faa3c4",
+    "d7154882474d665c6152ee3514e38882873f920ff5e9b528985eeb041d182708",
+    "45442e9eb10a79eef576a3a69d0fffb2611c2149f27dd86a4856bbe3d4c6be4c",
+    "b296c3a8b9331e6083370f34aa7504b4150e5f529a336bdd86906fcb4a52859c",
+    "aef21d825a42512e215c4893a48e80908cf9ad3f8c15165a8abfa71789eb9bf3",
+    "d58494c0a81aa928a4503e00fedd8e52ac6015a01628e080cd8ca6d2f29b9f90",
+    "9d0e28fbb7c37a5c5b83ad1eec81d1a33de60d3cc51075d32c193a5253b9170b",
 ]
 HIGHLIGHTED_WANG_MARKDOWN = re.compile(
     r"(?:`[^`]*(?:Wang|\u738b\u7136)[^`]*`|\*\*[^*]*(?:Wang|\u738b\u7136)[^*]*\*\*)"
@@ -91,8 +114,18 @@ class PublicationsContractTest(unittest.TestCase):
         self.assertEqual(
             EXPECTED_PUBLICATION_PROJECTION,
             [
-                tuple(publication.get(field) if isinstance(publication, dict) else None for field in ("badge", "image", "web_url", "pdf_url"))
+                tuple(publication.get(field) if isinstance(publication, dict) else None for field in ("badge", "image", "image_alt", "web_url", "pdf_url"))
                 for publication in publications
+            ],
+        )
+        self.assertEqual(
+            EXPECTED_CITATION_SHA256,
+            [
+                sha256(citation.encode("utf-8")).hexdigest() if isinstance(citation, str) else None
+                for citation in (
+                    publication.get("citation") if isinstance(publication, dict) else None
+                    for publication in publications
+                )
             ],
         )
 
